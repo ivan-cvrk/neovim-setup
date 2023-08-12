5967c256499421991b9afa5c2fb9ea9239c715f5
Ivan Cvrk <ivan.cvrk13@gmail.com>
2023-08-12 20:27:38 +0200

    Add descriptions to anonymous keybindings

--- lua/user/plugins/nvim-dap.lua
+++ lua/user/plugins/nvim-dap.lua
@@ -1,48 +1,94 @@
 -- dapui
 local dap, dapui = require('dap'), require('dapui')
 
-vim.keymap.set('n', '<F5>', function() dap.continue() end)
-vim.keymap.set('n', '<F6>', function() dap.step_over() end)
-vim.keymap.set('n', '<F7>', function() dap.step_into() end)
-vim.keymap.set('n', '<F8>', function() dap.step_out() end)
-vim.keymap.set('n', '<F9>', function() dap.terminate() end)
-vim.keymap.set('n', '\\b', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '\\B', function() dap.set_breakpoint() end)
-vim.keymap.set('n', '\\lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '\\dr', function() dap.repl.open() end)
-vim.keymap.set('n', '\\dl', function() dap.run_last() end)
-vim.keymap.set('n', '\\dp', function() dap.pause() end)
+vim.keymap.set('n', '<F5>', function() dap.continue() end, {
+    desc = 'Dap continue or start'
+})
+vim.keymap.set('n', '<F6>', function() dap.step_over() end, {
+    desc = 'Dap step over'
+})
+vim.keymap.set('n', '<F7>', function() dap.step_into() end, {
+    desc = 'Dap step into'
+})
+vim.keymap.set('n', '<F8>', function() dap.step_out() end, {
+    desc = 'Dap step out'
+})
+vim.keymap.set('n', '<F9>', function() dap.terminate() end, {
+    desc = 'Dap terminate'
+})
+vim.keymap.set('n', '\\b', function() dap.toggle_breakpoint() end, {
+    desc = 'Dap toggle breakpoint'
+})
+vim.keymap.set('n', '\\B', function() dap.set_breakpoint() end, {
+    desc = 'Dap set breakpoint'
+})
+vim.keymap.set('n', '\\lp',
+    function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, {
+    desc = 'Dap set log point'
+})
+vim.keymap.set('n', '\\dr', function() dap.repl.open() end, {
+    desc = 'Dap open repl'
+})
+vim.keymap.set('n', '\\dl', function() dap.run_last() end, {
+    desc = 'Dap last run'
+})
+vim.keymap.set('n', '\\dp', function() dap.pause() end, {
+    desc = 'Dap pause'
+})
 
 local widgets = require('dap.ui.widgets')
-vim.keymap.set({ 'n', 'v' }, '\\dh', function()
-    widgets.hover()
-end)
-vim.keymap.set({ 'n', 'v' }, '\\dp', function()
-    widgets.preview()
-end)
-vim.keymap.set('n', '\\df', function()
-    widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '\\ds', function()
-    widgets.centered_float(widgets.scopes)
-end)
-vim.keymap.set('n', '\\dt', function()
-    widgets.centered_float(widgets.threads)
-end)
+vim.keymap.set({ 'n', 'v' }, '\\dh', function() widgets.hover() end, {
+    desc = 'Dap widgets hover'
+})
+vim.keymap.set({ 'n', 'v' }, '\\dp', function() widgets.preview() end, {
+    desc = 'Dap widgets preview'
+})
+vim.keymap.set('n', '\\df', function() widgets.centered_float(widgets.frames) end, {
+    desc = 'Dap widgets frames'
+})
+vim.keymap.set('n', '\\ds', function() widgets.centered_float(widgets.scopes) end, {
+    desc = 'Dap widgets scopes'
+})
+vim.keymap.set('n', '\\dt', function() widgets.centered_float(widgets.threads) end, {
+    desc = 'Dap widgets threads'
+})
 
 vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#993939', bg = '#31353f' })
 vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = '#31353f' })
 vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#98c379', bg = '#31353f' })
 
-vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint',
-    numhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapBreakpointCondition',
-    { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapBreakpointRejected',
-    { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
-vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+vim.fn.sign_define('DapBreakpoint', {
+    text = '',
+    texthl = 'DapBreakpoint',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint'
+})
+vim.fn.sign_define('DapBreakpointCondition', {
+    text = 'ﳁ',
+    texthl = 'DapBreakpoint',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint'
+})
+vim.fn.sign_define('DapBreakpointRejected', {
+    text = '',
+    texthl = 'DapBreakpoint',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint'
+})
+vim.fn.sign_define('DapLogPoint', {
+    text = '',
+    texthl = 'DapLogPoint',
+    linehl = 'DapLogPoint',
+    numhl = 'DapLogPoint'
+})
+vim.fn.sign_define('DapStopped', {
+    text = '',
+    texthl = 'DapStopped',
+    linehl = 'DapStopped',
+    numhl = 'DapStopped'
+})
 
+-- Close dap widgets with q
 vim.cmd('autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>')
 
 local nvim_tree_open = false
@@ -56,7 +102,9 @@ local function on_exit()
     require 'nvim-dap-virtual-text'.refresh()
 end
 
-vim.keymap.set('n', '\\du', on_exit)
+vim.keymap.set('n', '\\du', on_exit, {
+    desc = 'Dap close UI'
+})
 
 dap.listeners.after.event_initialized.dapui_config = function()
     nvim_tree_open = require('nvim-tree.view').is_visible()
@@ -112,4 +160,3 @@ dap.configurations.cpp = {
 }
 
 dap.configurations.c = dap.configurations.cpp
-
--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -16,6 +16,12 @@ ntree.setup({
     }
 })
 
-vim.keymap.set("n", "<leader>mn", require("nvim-tree.api").marks.navigate.next)
-vim.keymap.set("n", "<leader>mp", require("nvim-tree.api").marks.navigate.prev)
-vim.keymap.set("n", "<leader>ms", require("nvim-tree.api").marks.navigate.select)
+vim.keymap.set("n", "<leader>mn", require("nvim-tree.api").marks.navigate.next, {
+    desc = "NvimTree go to next bookmark"
+})
+vim.keymap.set("n", "<leader>mp", require("nvim-tree.api").marks.navigate.prev, {
+    desc = "NvimTree go to previous bookmark"
+})
+vim.keymap.set("n", "<leader>ms", require("nvim-tree.api").marks.navigate.select, {
+    desc = "NvimTree bookmarks select"
+})
