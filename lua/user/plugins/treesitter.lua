38634a341da286ab352a49d9f06228e6d0793009
Ivan Cvrk <ivan.cvrk13@gmail.com>
2023-09-07 10:41:49 +0200

    Include autotag as TS plugin

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -8,10 +8,20 @@ configs.setup({
         enable = true, -- false will disable the whole extension
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enable = true,
+        disable = { 'html' }
+    },
+    autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+        filetypes = { "html", "xml" },
+    },
     autopairs = {
         enable = true,
     },
-    indent = { enable = true, disable = { 'html' } },
     rainbow = {
         enable = true,
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
