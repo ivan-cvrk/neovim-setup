ccd7031a8eff0af72bbb30d18bd198d3fe6b1f20
Ivan Cvrk <ivan.cvrk13@gmail.com>
2023-09-10 04:37:04 +0200

    Update autocompletion

--- lua/user/plugins/lsp/autocomplete.lua
+++ lua/user/plugins/lsp/autocomplete.lua
@@ -13,29 +13,30 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert({
         ['<C-d>'] = cmp.mapping.scroll_docs( -4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-y>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable( -1) then
-                luasnip.jump( -1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+--        ['<Tab>'] = cmp.mapping(function(fallback)
+--            if cmp.visible() then
+--                cmp.select_next_item()
+--            elseif luasnip.expand_or_jumpable() then
+--                luasnip.expand_or_jump()
+--            else
+--                fallback()
+--            end
+--        end, { 'i', 's' }),
+--        ['<S-Tab>'] = cmp.mapping(function(fallback)
+--            if cmp.visible() then
+--                cmp.select_prev_item()
+--            elseif luasnip.jumpable( -1) then
+--                luasnip.jump( -1)
+--            else
+--                fallback()
+--            end
+--        end, { 'i', 's' }),
     }),
     sources = {
         { name = 'nvim_lsp' },
@@ -54,5 +55,9 @@ cmp.setup {
                 buffer = '[buf]',
             }
         }
+    },
+    experimental = {
+        native_menu = false,
+        ghost_text = true
     }
 }
