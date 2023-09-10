ed85922cc0ca681d81152d2a1a4cc504adb911f8
Ivan Cvrk <ivan.cvrk13@gmail.com>
2023-09-10 02:47:39 +0200

    Add absolute dashboard colors

--- lua/user/plugins/dashboard.lua
+++ lua/user/plugins/dashboard.lua
@@ -1,57 +1,60 @@
 -- Setup module
 local dashboard = require('dashboard')
 
-vim.api.nvim_set_hl(0, 'DashboardHeader', { link = 'Function' })
-vim.api.nvim_set_hl(0, 'DashboardFooter', { link = 'String'} )
+vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#61afef' })
+vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#98c379' })
+
+vim.api.nvim_set_hl(0, 'DashboardLabel', { fg = '#c678dd' })
+vim.api.nvim_set_hl(0, 'DashboardIcon', { fg = '#e5c07b' })
+
 
 dashboard.setup {
-  theme = 'doom',
-  config = {
-    week_header = {
-      enable = true,
-    },
-    center = {
-      {
-         icon = '  ',
-         desc = 'Open      ',
-         action = 'exe \'NvimTreeOpen\' | exe \'wincmd p\' | exe \'q\' | lua vim.schedule(function() require(\'user.myfuncs\').execute_nvim_files_in_dir() end)',
-         icon_hl = 'Type',
-         desc_hl = 'Label',
-      },
-      {
-         icon = '  ',
-         desc = 'Find file    ',
-         action = 'Telescope find_files',
-         icon_hl = 'Type',
-         desc_hl = 'Label',
-      },
-      {
-         icon = '  ',
-         desc = 'Open project ',
-         action = 'Telescope project',
-         icon_hl = 'Type',
-         desc_hl = 'Label',
-      },
-      {
-         icon = '  ',
-         desc = 'Recent files ',
-         action = 'Telescope oldfiles',
-         icon_hl = 'Type',
-         desc_hl = 'Label',
-      },
-      {
-         icon = '  ',
-         desc = 'Config    ',
-         action = 'edit ' .. vim.fn.stdpath 'config',
-         icon_hl = 'Type',
-         desc_hl = 'Label',
-      },
-    },
-    footer = {
-      [[                                            ]],
-      [[ Do one thing, do it well - Unix philosophy ]],
-      [[                                            ]],
+    theme = 'doom',
+    config = {
+        week_header = {
+            enable = true,
+        },
+        center = {
+            {
+                icon = '  ',
+                desc = 'Open      ',
+                action = 'exe \'NvimTreeOpen\' | exe \'wincmd p\' | exe \'q\' | lua vim.schedule(function() require(\'user.myfuncs\').execute_nvim_files_in_dir() end)',
+                icon_hl = 'DashboardIcon',
+                desc_hl = 'DashboardLabel',
+            },
+            {
+                icon = '  ',
+                desc = 'Find file    ',
+                action = 'Telescope find_files',
+                icon_hl = 'DashboardIcon',
+                desc_hl = 'DashboardLabel',
+            },
+            {
+                icon = '  ',
+                desc = 'Open project ',
+                action = 'Telescope project',
+                icon_hl = 'DashboardIcon',
+                desc_hl = 'DashboardLabel',
+            },
+            {
+                icon = '  ',
+                desc = 'Recent files ',
+                action = 'Telescope oldfiles',
+                icon_hl = 'DashboardIcon',
+                desc_hl = 'DashboardLabel',
+            },
+            {
+                icon = '  ',
+                desc = 'Config    ',
+                action = 'edit ' .. vim.fn.stdpath 'config',
+                icon_hl = 'DashboardIcon',
+                desc_hl = 'DashboardLabel',
+            },
+        },
+        footer = {
+            [[                                            ]],
+            [[ Do one thing, do it well - Unix philosophy ]],
+            [[                                            ]],
+        }
     }
-  }
 }
-
