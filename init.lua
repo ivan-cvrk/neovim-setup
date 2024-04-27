2e1b28d8e77868e7aafd0002191d499f87a87811
Ivan Cvrk <ivan.cvrk13@gmail.com>
2024-04-27 19:46:35 +0200

    Set Neovide transparency

--- init.lua
+++ init.lua
@@ -21,6 +21,8 @@ require("lazy").setup({
 }, { ui = { title = 'Lazy', border = "rounded" } })
 
 if vim.g.neovide then
-    vim.o.guifont = 'JetBrainsMonoNL Nerd Font:h16:e-antialiasing'
-    vim.g.neovide_cursor_vfx_mode = "torpedo"
+    vim.o.guifont = 'JetBrainsMonoNL Nerd Font:h15:e-antialiasing'
+    vim.g.neovide_hide_cursor_when_typing = true
+    vim.g.neovide_theme = 'auto'
+    vim.g.neovide_cursor_vfx_mode = 'torpedo'
 end
