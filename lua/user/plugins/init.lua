ed963f14a04c10ffdc604280a1670ebedc2a222d
Ivan Cvrk <ivan.cvrk13@gmail.com>
2024-05-09 12:36:37 +0200

    Remove latex dependencies

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -49,17 +49,4 @@ return {
       }
     },
   },
-
-  ----------------------
-  --      LaTex      ---
-  ----------------------
-
-  {
-    'lervag/vimtex',
-    lazy = true,
-    ft = 'tex',
-    init = function()
-      vim.g.vimtex_view_method = 'zathura'
-    end
-  },
 }
