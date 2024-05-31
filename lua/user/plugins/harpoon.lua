64f1847479cbab6ad0b434d99da57d5f68fc3c3c
Ivan Cvrk <ivan.cvrk13@gmail.com>
2024-05-31 19:21:53 +0200

    Fix harpoon keymaps on qf

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -9,13 +9,46 @@ return {
     local harpoon_ui = require('harpoon.ui')
     local harpoon_term = require('harpoon.term')
 
-    vim.keymap.set('n', '<leader>a', function() harpoon_mark.add_file() end)
     vim.keymap.set('n', '<C-e>', function() harpoon_ui.toggle_quick_menu() end)
+    local harpoon_au = vim.api.nvim_create_augroup('HarpoonKeymaps', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+      group = harpoon_au,
+      pattern = '*',
+      callback = function(opt)
+        if vim.bo.filetype ~= 'qf' then
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<leader>a', '',
+            {
+              callback = function() harpoon_mark.add_file() end,
+              desc = 'Harpoon mark file'
+            })
 
-    vim.keymap.set('n', '<C-h>', function() harpoon_ui.nav_file(1) end)
-    vim.keymap.set('n', '<C-j>', function() harpoon_ui.nav_file(2) end)
-    vim.keymap.set('n', '<C-m>', function() harpoon_ui.nav_file(3) end)
-    vim.keymap.set('n', '<C-s>', function() harpoon_ui.nav_file(4) end)
-    vim.keymap.set('n', '<C-t>', function() harpoon_term.gotoTerminal(1) end)
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<C-h>', '',
+            {
+              callback = function() harpoon_ui.nav_file(1) end,
+              desc = 'Harpoon jump first'
+            })
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<C-j>', '',
+            {
+              callback = function() harpoon_ui.nav_file(2) end,
+              desc = 'Harpoon jump first'
+            })
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<C-m>', '',
+            {
+              callback = function() harpoon_ui.nav_file(3) end,
+              desc = 'Harpoon jump third'
+            })
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<C-s>', '',
+            {
+              callback = function() harpoon_ui.nav_file(4) end,
+              desc = 'Harpoon jump fourth'
+            })
+          vim.api.nvim_buf_set_keymap(opt.buf, 'n', '<C-t>', '',
+            {
+              callback = function() harpoon_term.gotoTerminal(1) end,
+              desc = 'Harpoon open terminal'
+            })
+        end
+      end
+    })
   end
 }
