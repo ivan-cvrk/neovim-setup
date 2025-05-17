f9557fc94886eff1cb454029432826e67af22166
Ivan Cvrk <ivan.cvrk13@gmail.com>
2025-05-17 12:12:39 +0200

    Update plugins

--- init.lua
+++ init.lua
@@ -33,11 +33,13 @@ if vim.g.neovide then
   vim.g.neovide_floating_blur_amount_x = 3.0
   vim.g.neovide_floating_blur_amount_y = 3.0
 
+  vim.api.nvim_set_option_value('winblend', 20, {scope = 'global'})
+
   -- Helper function for transparency formatting
   local alpha = function()
     return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
   end
-  vim.g.neovide_transparency = 0.9 -- floating transparency
+  vim.g.neovide_transparency = 1.0 -- floating transparency
   vim.g.transparency = 0.8 -- macos transparency
   vim.g.neovide_background_color = "#0f1117" .. alpha()
 
@@ -46,7 +48,8 @@ if vim.g.neovide then
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
 
-  vim.g.neovide_normal_opacity = 0.95
+  vim.g.neovide_opacity = 0.5
+  vim.g.neovide_normal_opacity = 0.9
 
   vim.g.neovide_theme = 'auto'
 
--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -87,9 +87,9 @@ return {
           workspace = {
             checkThirdParty = false,
             library = {
-              vim.env.VIMRUNTIME
+              vim.env.VIMRUNTIME,
               -- Depending on the usage, you might want to add additional paths here.
-              -- "${3rd}/luv/library"
+              -- "${3rd}/luv/library",
               -- "${3rd}/busted/library",
             }
             -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
