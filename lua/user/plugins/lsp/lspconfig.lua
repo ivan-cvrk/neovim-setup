9b859d2da1d3cfae6fff263c1029edea70e5815f
Ivan Cvrk <ivan.cvrk13@gmail.com>
2025-06-02 23:14:44 +0200

    Remove <C_M> lsp binding in favour of K

--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -30,7 +30,6 @@ return {
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'M', vim.lsp.buf.hover, bufopts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
         vim.keymap.set('n', '<space>m', vim.lsp.buf.signature_help, bufopts)
